f059fce6409c2d2cacf876b1047ebfc250554ae3
Olubukola <19bukola97@gmail.com>
2023-05-30 08:18:31 +0100

    () added to the toDateString method

--- src/App.js
+++ src/App.js
@@ -28,17 +28,17 @@ function App() {
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
-        date={expenses[0].date.toDateString}
+        date={expenses[0].date.toDateString()}
       ></ExpenseItem>
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
-        date={expenses[1].date.toDateString}
+        date={expenses[1].date.toDateString()}
       ></ExpenseItem>
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
-        date={expenses[2].date.toDateString}
+        date={expenses[2].date.toDateString()}
       ></ExpenseItem>
     </div>
   );
